7554dc1c33da7b4dc3640d05bdaf1ccc1c60d4a3
Your Name <you@example.com>
2025-09-10 20:28:16 +0800

    v1.1

--- ps.sh
+++ ps.sh
@@ -4,7 +4,7 @@
 # Git 用户名和密码（⚠️明文存储，不安全）
 USERNAME="fank1101"
 PASSWORD="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"
-REPO="https://github.com/fank1101/argocd.git"
+REPO="github.com/fank1101/argocd.git"
 
 # 提交信息
 commit_msg=$1
